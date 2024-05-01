dbbb2361f21e307ad157180a3f87ef033b32f1a7
vojone <vvd1@centrum.cz>
2024-05-01 14:17:01 +0200

    Upgraded bootstrap.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 # Script for initialization of environment
 # Requires python3 and pip commands to be available.
+#
+# USAGE: . ./bootstrap.sh
 
 ENV_NAME="env"
 HAL_CGP_SOURCE="https://github.com/Happy-Algorithms-League/hal-cgp.git"
@@ -10,19 +12,29 @@ REQUIREMENTS_PATH="src/requirements.txt"
 
 if [ ! -d "$ENV_NAME" ]; then
     echo "Environment '$ENV_NAME' not found..."
+    echo "Creating the new environment..."
 
     python3 -m venv "$ENV_NAME" || exit
 
+    # shellcheck disable=SC1090
+    . "./$ENV_NAME/bin/activate"
+
+    # There is some dependecy error, so we have to install hal-cgp manually
     git clone "$HAL_CGP_SOURCE" "$HAL_CGP_DIR"
     pushd "$HAL_CGP_DIR" || exit
     pip install .
 
+    popd || exit
+
     pip install -r "$REQUIREMENTS_PATH"
+
+    echo "Setup done."
+else
+    echo "Environment found... Activating..."
+
+    # shellcheck disable=SC1090
+    . "./$ENV_NAME/bin/activate"
+
+    echo "Done."
 fi
-
-
-echo "Environment prepared... Activating..."
-
-# shellcheck disable=SC1090
-. "./$ENV_NAME/bin/activate"
 
