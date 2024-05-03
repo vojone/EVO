952f597c5842ee62ee5a127a5bd7f3ba1ed68715
vojone <vvd1@centrum.cz>
2024-05-03 10:18:07 +0200

    Fixed path in some jobscripts

--- scripts/job5.sh
+++ scripts/job5.sh
@@ -4,12 +4,12 @@
 
 pushd src || exit
 
-python3 filter_cgp.py ../experiments/size/gaus_window_cross.json >>"$MSTATUS" &
-python3 filter_cgp.py ../experiments/size/gaus_window_3d.json >>"$MSTATUS" &
-python3 filter_cgp.py ../experiments/size/gaus_window_3x1.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/gaus_window_cross.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/gaus_window3d.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/gaus_window3x1.json >>"$MSTATUS" &
 
-python3 filter_cgp.py ../experiments/size/line_window_cross.json >>"$MSTATUS" &
-python3 filter_cgp.py ../experiments/size/line_window_3d.json >>"$MSTATUS" &
-python3 filter_cgp.py ../experiments/size/line_window_3x1.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/line_window_cross.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/line_window3d.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/line_window3x1.json >>"$MSTATUS" &
 
 wait
--- scripts/job6.sh
+++ scripts/job6.sh
@@ -4,12 +4,12 @@
 
 pushd src || exit
 
-python3 filter_cgp.py ../experiments/size/lined_window_cross.json >>"$MSTATUS" &
-python3 filter_cgp.py ../experiments/size/lined_window_3d.json >>"$MSTATUS" &
-python3 filter_cgp.py ../experiments/size/lined_window_3x1.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/lined_window_cross.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/lined_window3d.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/lined_window3x1.json >>"$MSTATUS" &
 
-python3 filter_cgp.py ../experiments/size/sp_window_cross.json >>"$MSTATUS" &
-python3 filter_cgp.py ../experiments/size/sp_window_3d.json >>"$MSTATUS" &
-python3 filter_cgp.py ../experiments/size/sp_window_3x1.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/sp_window_cross.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/sp_window3d.json >>"$MSTATUS" &
+python3 filter_cgp.py ../experiments/window/sp_window3x1.json >>"$MSTATUS" &
 
 wait
